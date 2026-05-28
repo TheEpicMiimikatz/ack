178eef70cb40868f5dbe5547ed3f9132587aab23
TheEpicMiimikatz <theepicmimikatz@tutamail.com>
2026-05-28 19:37:24 +0000

    Create auth.php

---
+++ server/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $token=$_SERVER['HTTP_X_API_TOKEN'];
+
+    if ($token != '<CUSTOM-API-TOKEN-HERE>') {
+        header('Content-Type: application/json', true, 401);
+        echo json_encode(['status' => 'error', 'reason' => 'unauthorized']);
+        exit;
+    }
+
+    $bot_id = uniqid();
+
+    setcookie('.auth_id', $bot_id);
+}
